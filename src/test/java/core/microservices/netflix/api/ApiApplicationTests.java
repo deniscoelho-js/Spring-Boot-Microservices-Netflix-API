e9113b1b8235b614f6ed7ae9ca4492a50ac45c8b
Denis Coelho <deniscoelho.dev@gmail.com>
2023-08-07 21:32:41 -0300

    first commit

---
+++ src/test/java/core/microservices/netflix/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package core.microservices.netflix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
