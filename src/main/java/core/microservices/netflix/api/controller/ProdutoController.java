c73676e013fa6b9130e4615acb2b487823cbaa3f
Denis Coelho <deniscoelho.dev@gmail.com>
2023-08-09 22:54:48 -0300

    atualizacao

---
+++ src/main/java/core/microservices/netflix/api/controller/ProdutoController.java
@@ -0,0 +1,23 @@
+package core.microservices.netflix.api.controller;
+
+import core.microservices.netflix.api.model.Produto;
+import core.microservices.netflix.api.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public List<Produto> obterTodos(){
+        return produtoService.obterTodos();
+    }
+}
